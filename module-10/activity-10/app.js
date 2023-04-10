fd08bbc757ac3b6f9422340c5b73b9a2a73236d7
SimranjotKaur <taneja.simranjot@gmail.com>
2023-04-09 23:55:36 -0400

    assignment files added

--- module-10/activity-10/app.js
+++ module-10/activity-10/app.js
@@ -9,65 +9,91 @@ const getToken = async (clientId, clientSecret) => {
             Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
         },
         body: "grant_type=client_credentials",
-    })
+    });
 
     const data = await result.json();
     return data.access_token;
-}
+};
 
 const loadData = async (clientId, clientSecret) => {
     const token = await getToken(clientId, clientSecret);
     const categories = await getCategory(token);
     const root = document.getElementById(`root`);
-    categories.map(async ({name, id, icons:[icon], href}) => {
+    for (const { name, id, icons: [icon], href } of categories) {
         const playlistData = await getPlaylistByCategory(token, id);
-        const playlists = playlistData.map(({ name, external_urls: { spotify }, images: [image] }) => 
-            `<li>
-                <a href="${spotify}" alt="${name}" target="_blank">
-                    <img src="${image.url}" width="180" height="180" />
-                </a>
-            </li>`
-        ).join(``);
+        const html = `
+      <article class="category-card">
+        <div class="category">
+          <div class="category--div">
+            <img
+              class="image-category"
+              src="${icon.url}"
+              width="${icon.width}"
+              height="${icon.height}"
+              alt="${name}"
+            />
+            <h4 class="category--h4">${name}</h4>
+          </div>
+          <div class="playlists">
+            <ol>${await getPlaylists(playlistData, token)}</ol>
+          </div>
+        </div>
+      </article>
+    `;
+        root.insertAdjacentHTML("beforeend", html);
+    }
+};
 
-        if (playlists) {
-            const html = `
-                <article class="category-card">
-                    <div class="category">
-                        <div class="category--div">
-                            <img class="image-category" src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
-                            <h4 class="category--h4">${name}</h4>
-                        </div>
-                        <div class="playlists">
-                            <ol>${playlists}</l>
-                        </div>
-                    </div>
-                </article>
-            `
-            root.insertAdjacentHTML("beforeend", html);  
-        }
-    });
-}
+const getPlaylists = async (playlistData, token) => {
+    const playlistItems = await Promise.all(
+        playlistData.map(async ({ name, external_urls: { spotify }, images: [image], tracks: { href } }) => {
+            const tracks = await getTracksByPlaylist(token, href);
+            const trackList = tracks.map(({ track: { name, artists, external_urls: { spotify } } }) => `<li><a href=${spotify}>${name} - ${artists.map(artist => artist.name).join(', ')}</a></li>`).join('');
+            return `
+        <li>
+          <a href="${spotify}" alt="${name}" target="_blank">
+            <img src="${image.url}" width="180" height="180" />
+          </a>
+          <details>
+          <summary>Tracks</summary>
+          <div>
+            <ol class="tracks">${trackList}</ol>
+          </div>
+        </details>
+        </li>
+      `;
+        })
+    );
+    return playlistItems.join('');
+};
 
 const getCategory = async (token) => {
-    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_IN&limit=10`,
-        {
-            method: "GET",
-            headers: { Authorization: "Bearer " + token },
-
-        });
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_IN&limit=10`, {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+    });
 
     const data = await result.json();
     return data.categories.items;
-}
+};
 
 const getPlaylistByCategory = async (token, categoryId) => {
-    const result = await fetch(`https://api.spotify.com/v1/browse/categories/${categoryId}/playlists?country=IN&limit=10`,
-        {
-            method: "GET",
-            headers: { Authorization: "Bearer " + token }
-        })
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories/${categoryId}/playlists?country=IN&limit=10`, {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+    });
     const data = await result.json();
     return data.playlists.items;
+}
+
+const getTracksByPlaylist = async (token, href) => {
+    const result = await fetch(href, {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+    });
+
+    const data = await result.json();
+    return data.items;
 }
 
 loadData(clientId, clientSecret);
--- module-10/assignments/assignment.md
+++ module-10/assignments/assignment.md
@@ -3,3 +3,6 @@
 - Expend your activity to also show tracks for playlists
 - Each playlist object has a field `tracks` that has the endpoint to load tracks. display at least track name and artist
 - Attach screenshots of your page
+
+!["Track List Collapsed"](../activity-10/assignment-output2.png)
+!["Track List Expanded"](../activity-10/assignment-output1.png)
